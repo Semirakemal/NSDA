29a3b586f6c20672d7ef9ef411d0171bc55eca5a
Semirakemal <Ksemira225@gmail.com>
2025-08-06 22:50:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+
+      <h1>Hello</h1>
+      <p>Start editing to  
+        :)</p>
+
+        
+    </div>
+  );
+}
